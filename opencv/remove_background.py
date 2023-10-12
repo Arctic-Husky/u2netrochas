5a4e8948101b819ce60d7ceca54152b26a0f11c9
arctichusky <caiofonseca6@gmail.com>
2023-10-12 14:56:24 -0300

    Corrige file path

--- opencv/remove_background.py
+++ opencv/remove_background.py
@@ -11,14 +11,14 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
+print("Removendo backgrounds")
 
-
-images = load_images_from_folder('Images')
-masks = load_images_from_folder('Masks')
+images = load_images_from_folder('../test_data/test_images')
+masks = load_images_from_folder('../test_data/u2net_bce_itr_15656_train_0.230484_tar_0.023406_results')
 
 i = 0
 while i < len(images):
     final = cv2.bitwise_and(images[i], masks[i])
-    cv2.imwrite("Final_Images/{}.png".format(i), final)
+    cv2.imwrite("../final_images/image{}.png".format(i), final)
     i = i + 1
 
