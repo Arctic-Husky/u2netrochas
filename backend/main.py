e15f76c61f2af884a82067b71eebd86ee6c96032
Caio Schmidt <caiofonseca6@gmail.com>
2023-10-27 07:10:47 -0300

    Adiciona backend

---
+++ backend/main.py
@@ -0,0 +1,36 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import uuid
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+class Rocha(BaseModel):
+    id: uuid
+    nome_arquivo: str
+    url_arquivo: str
+
+# Banco: Supabase https://supabase.com/dashboard/project/yjdjcvftedrffiiidcyb
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
