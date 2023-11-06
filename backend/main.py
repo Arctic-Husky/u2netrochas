89efc3f7b45b342de27d23f186739d264d762803
Caio Schmidt <caiofonseca6@gmail.com>
2023-11-06 07:46:54 -0300

    Adiciona uploadfile

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 
-import uuid
+import json
+# import uuid
 
 app = FastAPI()
 
@@ -13,10 +14,10 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
-class Rocha(BaseModel):
-    id: uuid
-    nome_arquivo: str
-    url_arquivo: str
+# class Rocha(BaseModel):
+#     id: uuid
+#     nome_arquivo: str
+#     url_arquivo: str
 
 # Banco: Supabase https://supabase.com/dashboard/project/yjdjcvftedrffiiidcyb
 
@@ -25,12 +26,17 @@ class Rocha(BaseModel):
 async def read_root():
     return {"Hello": "World"}
 
+@app.post("/upload")
+async def upload_file(file: UploadFile):
+    data = json.load(file.file.read())
+    return {"filename": file.filename}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
